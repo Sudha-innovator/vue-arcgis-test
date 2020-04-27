9519d72f13decceaec65829a50498fb7265cdbf0
Sudha Panchaparvala <Sudha.Panchaparvala@arup.com>
2020-04-27 15:36:11 +1000

    unwanted code

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -19,7 +19,7 @@ export default {
           required: true
       }
 
-  }, */
+  }, 
     computed: {
     countryname() {
         return this.$store.getters.getName;
@@ -33,7 +33,7 @@ export default {
         this.$store.dispatch('setName', val);
     }
 
-    },
+    },*/
 
   mounted() {
 
